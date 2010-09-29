e7ddea6603c04bbd4ff05966066ae20d31ead503
Geoff Drake <geoffd@professionalhosts.com>
2010-09-29 10:38:00 +0200

    Removed unused constant.

--- src/net/threescale/api/LogFactory.java
+++ src/net/threescale/api/LogFactory.java
@@ -4,8 +4,6 @@ import java.util.logging.*;
 
 public class LogFactory {
 
-	public static final String LOG_NAME = "net.treescale.log";
-	
 	public static Logger getLogger(Object clazz) {
 		return Logger.getLogger(clazz.getClass().getName());
 	}
