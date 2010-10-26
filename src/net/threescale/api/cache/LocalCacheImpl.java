6fcc654789d555da8d920a68dd596cc7441eb4a4
Geoff Drake <geoffd@professionalhosts.com>
2010-10-26 17:04:41 +0200

    Implemented test and code for basic local cache

--- src/net/threescale/api/cache/LocalCacheImpl.java
+++ src/net/threescale/api/cache/LocalCacheImpl.java
@@ -1,15 +1,34 @@
 package net.threescale.api.cache;
 
 import net.threescale.api.v2.AuthorizeResponse;
+import org.jboss.cache.*;
 
 
 public class LocalCacheImpl implements ApiCache {
-    
-    public AuthorizeResponse getAuthorizeFor(String userKey) {
-        return null;
+
+    private static final String authorize_prefix = "authorize";
+    private static final String responseKey = "response";
+
+    private Cache cache;
+
+    public LocalCacheImpl() {
+
+        CacheFactory factory = new DefaultCacheFactory();
+        cache = factory.createCache();
+    }
+
+    public AuthorizeResponse getAuthorizeFor(String app_key) {
+        Fqn authorizeFqn = Fqn.fromString(authorize_prefix + "/" + app_key);
+        return (AuthorizeResponse)cache.get(authorizeFqn, responseKey);
     }
 
     public void addAuthorizedResponse(String app_key, AuthorizeResponse authorizedResponse) {
-
+        Fqn authorizeFqn = Fqn.fromString(authorize_prefix + "/" + app_key);
+        Node root = cache.getRoot();
+        Node authorizeNode = cache.getNode(authorizeFqn);
+        if (authorizeNode == null) {
+            authorizeNode = root.addChild(authorizeFqn);
+        }
+        authorizeNode.put(responseKey, authorizedResponse);
     }
 }
