76f29e1fd4d9ddfdf105786b867cc1c74816175e
Geoff Drake <geoffd123@hotmail.com>
2011-02-26 08:54:26 +0100

    Test for multiple reports.

--- src/test/java/net/threescale/api/cache/ReportCachingTest.java
+++ src/test/java/net/threescale/api/cache/ReportCachingTest.java
@@ -101,6 +101,16 @@ public class ReportCachingTest extends CommonBase {
         verify(sender).sendPostToServer(SERVER_URL, RESPONSE_DATA);
     }
 
+    @Test
+    public void multipleReportsTransactionsAreSentToServerAsOneMessage() throws Exception {
+        api_cache.setReportExpirationInterval(5L);
+        api_cache.report(createTransactionData());
+        api_cache.report(createSecondSetOfTransactionData());
+        Thread.sleep(800L);
+
+        verify(sender).sendPostToServer(SERVER_URL, MULTIPLE_RESPONSE_DATA);
+    }
+
     protected static final String RESPONSE_DATA =
             "provider_key=" + PROVIDER_KEY + "&" +
                     "transactions[0][app_id]=bad7e480&" +
@@ -111,6 +121,25 @@ public class ReportCachingTest extends CommonBase {
                     "transactions[1][usage][transfer]=4500&" +
                     "transactions[1][usage][hits]=1&" +
                     "transactions[1][timestamp]=2009-01-01+14%3A23%3A08";
+
+    protected static final String MULTIPLE_RESPONSE_DATA =
+            "provider_key=" + PROVIDER_KEY + "&" +
+            "transactions[0][app_id]=bad7e443&" +
+            "transactions[0][usage][transfer]=2800&" +
+            "transactions[0][usage][hits]=11&" +
+            "transactions[0][timestamp]=2009-01-01+18%3A11%3A58&" +
+            "transactions[1][app_id]=bad7e480&" +
+            "transactions[1][usage][transfer]=2840&" +
+            "transactions[1][usage][hits]=1&" +
+            "transactions[1][timestamp]=2009-01-01+18%3A11%3A59&" +
+            "transactions[2][app_id]=bce4c8f4&"  +
+            "transactions[2][usage][transfer]=4500&" +
+            "transactions[2][usage][hits]=1&" +
+            "transactions[2][timestamp]=2009-01-01+14%3A23%3A08&" +
+            "transactions[3][app_id]=bce4c8f4&" +
+            "transactions[3][usage][transfer]=1500&" +
+            "transactions[3][usage][hits]=4&" +
+            "transactions[3][timestamp]=2009-01-01+14%3A23%3A20";
 
     private ApiTransaction[] createTransactionData() {
         ApiTransaction[] transactions = new ApiTransaction[2];
@@ -127,5 +156,19 @@ public class ReportCachingTest extends CommonBase {
         return transactions;
     }
 
+    private ApiTransaction[] createSecondSetOfTransactionData() {
+        ApiTransaction[] transactions = new ApiTransaction[2];
+        HashMap<String, String> metrics0 = new HashMap<String, String>();
+        metrics0.put("hits", "4");
+        metrics0.put("transfer", "1500");
+
+        HashMap<String, String> metrics1 = new HashMap<String, String>();
+        metrics1.put("hits", "11");
+        metrics1.put("transfer", "2800");
+
+        transactions[0] = new ApiTransaction("bce4c8f4", "2009-01-01 14:23:20", metrics0);
+        transactions[1] = new ApiTransaction("bad7e443", "2009-01-01 18:11:58", metrics1);
+        return transactions;
+    }
 
 }
