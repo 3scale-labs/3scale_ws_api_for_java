278d15202c226dd08d909bbb5d7ec597988a196d
Geoff Drake <geoffd123@hotmail.com>
2011-07-27 14:38:00 +0200

    Removed Version 1 API.

--- src/main/java/net/threescale/api/ApiFactory.java
+++ src/main/java/net/threescale/api/ApiFactory.java
@@ -13,29 +13,6 @@ public class ApiFactory {
     public static String DEFAULT_3SCALE_PROVIDER_API_URL = "http://su1.3scale.net";
 
     /**
-     * Creates a new Api object.
-     *
-     * @param url                  URL of the server to connect to. e.g. http://server.3scale.net.
-     * @param provider_private_key The Providers private key obtained from 3scale.
-     * @return A new Api object.
-     */
-    public static net.threescale.api.Api createApi(String url, String provider_private_key) {
-        return new ApiImpl(url, provider_private_key);
-    }
-
-    /**
-     * Creates a new Api object.
-     *
-     * @param url                  URL of the server to connect to. e.g. http://server.3scale.net.
-     * @param provider_private_key The Providers private key obtained from 3scale.
-     * @param sender               The HttpSender object to be used for communication with the server.
-     * @return A new Api object.
-     */
-    public static net.threescale.api.Api createApi(String url, String provider_private_key, net.threescale.api.HttpSender sender) {
-        return new ApiImpl(url, provider_private_key, sender);
-    }
-
-    /**
      * Creates a new Version 2 Api object.
      *
      * @param url                  URL of the server to connect to. e.g. http://su1.3scale.net.
