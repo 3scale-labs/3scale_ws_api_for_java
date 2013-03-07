c11a5f532130074cd211de4d37ed3f0c16a40f3a
Joaquin Rivera Padron <joahking@gmail.com>
2013-03-07 18:19:24 +0100

    all calls examples for API key (user key) added

--- src/main/java/threescale/v3/api/example/AllCallsExample.java
+++ src/main/java/threescale/v3/api/example/AllCallsExample.java
@@ -15,21 +15,86 @@ public class AllCallsExample implements TestKeys {
 
     public static void main(String[] args) {
 
-        runAuthRep();
-        runOAuth();
-        runReport();
+        runUserKey();
+        //runAppId();
+        //runOAuth();
     }
 
     /**
-     * Example code for an AuthRep
+     * Example code for calls on user key (API key) mode
      */
-    private static void runAuthRep() {
+    private static void runUserKey() {
+        ServiceApi serviceApi = new ServiceApiDriver(my_provider_key);    // Create the API object
+
+        ParameterMap params = new ParameterMap();
+        params.add("user_key", user_key);                                 // Add keys for authrep o authorize
+        params.add("service_id", user_key_service_id);
+
+        ParameterMap usage = new ParameterMap();                          // Add a metric
+        usage.add("hits", "1");
+
+        params.add("usage", usage);
+
+        AuthorizeResponse response = null;
+        // the 'preferred way': authrep
+        try {
+            response = serviceApi.authrep(params);
+            System.out.println("AuthRep on User Key Success: " + response.success());
+            if (response.success() == false) {
+                System.out.println("Error: " + response.getErrorCode());
+                System.out.println("Reason: " + response.getReason());
+            }
+            System.out.println("Plan: " + response.getPlan());
+        } catch (ServerError serverError) {
+            serverError.printStackTrace();
+        }
+
+        // the '2 steps way': authorize + report
+        try {
+            response = serviceApi.authorize(params);
+            System.out.println("Authorize on User Key Success: " + response.success());
+            if (response.success() == false) {
+                System.out.println("Error: " + response.getErrorCode());
+                System.out.println("Reason: " + response.getReason());
+            } else {
+
+              // the API call got authorized, let's do a report
+              ParameterMap transaction = new ParameterMap();
+              transaction.add("user_key", user_key);
+
+              ParameterMap transaction_usage = new ParameterMap();
+              transaction_usage.add("hits", "1");
+              transaction.add("usage", transaction_usage);
+
+              try {
+                  final ReportResponse report_response = serviceApi.report(user_key_service_id, transaction);
+
+                  if (report_response.success()) {
+                      System.out.println("Report on User Key was successful");
+                  } else {
+                      System.out.println("Report on User Key failed");
+                  }
+              } catch (ServerError serverError) {
+                  serverError.printStackTrace();
+              }
+
+            }
+            System.out.println("Plan: " + response.getPlan());
+
+        } catch (ServerError serverError) {
+            serverError.printStackTrace();
+        }
+    }
+
+    /**
+     * Example code for an AuthRep on app id mode
+     */
+    private static void runAppId() {
         ServiceApi serviceApi = new ServiceApiDriver(my_provider_key);    // Create the API object
 
         ParameterMap params = new ParameterMap();                         // Add keys for the autorize
-        params.add("app_key", app_key);
-        params.add("user_key", user_key);
-        params.add("service_id", service_id);
+        params.add("app_id", app_id);
+        params.add("service_id", app_id_service_id);
 
         ParameterMap usage = new ParameterMap();                          // Add a hit metric
         usage.add("hits", "1");
