5977c4e611abf06c23bb3f945a32bcbf62973d78
Joaquin Rivera Padron <joahking@gmail.com>
2013-03-08 16:56:55 +0100

    TestKeysExample updated to latest AllCallsExample

--- src/main/java/threescale/v3/api/example/TestKeysExample.java
+++ src/main/java/threescale/v3/api/example/TestKeysExample.java
@@ -4,18 +4,18 @@ package threescale.v3.api.example;
  * Copy this interface to TestKeys and edit it replacing the values with your live values.
  */
 public interface TestKeysExample {
-
     static String my_provider_key = "<provider key>";
-    static String app_id = "<app id>";
-    static String app_key = "<app key>";
-    static String service_id = "<service id>";
-    static String user_key = "<user key>";
+
+    //App Id
+    static String app_id            = "<app id>";
+    static String app_key           = "<app key>";
+    static String app_id_service_id = "<app id service id>";
+
+    //API Key
+    static String user_key            = "<user key>";
+    static String user_key_service_id = "<user key service id>";
 
     // OAuth
-    static String client_id = "<client id>";
-    static String secret = "<secret key>";
-    static String oa_service_id = "<service id for oauth test>";
-    static String oa_app_id = "<app id for oauth test>";
-
-
+    static String oauth_service_id = "<oauth service id>";
+    static String oauth_app_id     = "<oauth app id>";
 }
