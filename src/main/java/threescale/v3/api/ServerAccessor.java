23f9ac6c6013c5a97a49b41d7ca72cf2777e0a39
Your Name <you@example.com>
2014-07-23 17:43:05 +0200

    Issue https://github.com/3scale/puppet/issues/284 -  Adding user client header with Integration test

--- src/main/java/threescale/v3/api/ServerAccessor.java
+++ src/main/java/threescale/v3/api/ServerAccessor.java
@@ -4,13 +4,14 @@ package threescale.v3.api;
  * Interface to HTML operation between this client and the 3Scale Server
  */
 public interface ServerAccessor {
-
+	String X_3SCALE_USER_CLIENT_HEADER = "X-3scale-user-client";
+	String X_3SCALE_USER_CLIENT_HEADER_JAVA_PLUGIN = "plugin-java";
     /**
      * Perform and HTML GET with the provided URL
      *
      * @param url The URL and parameters to be sent
      * @return Status and content as a HtmlResponse
-     * @throws ServerError If there are problems connection tp the server.
+     * @throws ServerError If there are problems connection tp the server. 
      */
     public HttpResponse get(String url) throws ServerError;
 
