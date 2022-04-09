fde6dca1973039e1e763668a8f40e85515fd2689
sachin <sachin.inguva@gmail.com>
2022-04-09 15:16:50 -0400

    Issue # 8 : MethodArgumentCouldBeFinal

--- src/main/java/threescale/v3/api/HttpResponse.java
+++ src/main/java/threescale/v3/api/HttpResponse.java
@@ -14,6 +14,7 @@ public class HttpResponse {
      * @param status Http Status
      * @param body   Http Body
      */
+    //FIXME: MethodArgumentCouldBeFinal
     public HttpResponse(int status, String body) {
         this.status = status;
         this.body = body;
